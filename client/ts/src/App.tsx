33e18f3210c60bd049657ea5724664ade9794adc
d0riven <wega315@gmail.com>
2020-02-28 01:06:47 +0900

    request api

--- client/ts/src/App.tsx
+++ client/ts/src/App.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
+import {DefaultApi, GetUserOutput, GetUserRequest} from "./api-client";
 
-export default class App extends React.Component<any, any>{
-    render() {
-        return (
-            <p>hello</p>
-        );
+class GetUserInput implements GetUserRequest {
+    constructor(public id:number) {
     }
 }
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <button onClick={() => {
+                    let c:DefaultApi = new DefaultApi();
+                    c.getUser(new GetUserInput(1)).then((v:GetUserOutput) => {
+                        console.log(v);
+                    });
+                }}>hello
+                </button>
+                <p>hello</p>
+            </div>
+        );
+    }
+};
